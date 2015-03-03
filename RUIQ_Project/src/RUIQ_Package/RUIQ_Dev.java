3367acab81d400dfd6fe8a0b4cd269f14d1400f7
skLaFarebear <awadallah.farris@gmail.com>
2015-03-02 19:06:49 -0500

    fix revert

---
+++ RUIQ_Project/src/RUIQ_Package/RUIQ_Dev.java
@@ -0,0 +1,13 @@
+package RUIQ_Package;
+
+public class RUIQ_Dev {
+
+	public static void main(String[] args) {
+		
+		//stufffffffff
+		//djsfnskjdnfdsfnsdjkfnas
+		System.out.print("stuff");
+
+	}
+
+}
